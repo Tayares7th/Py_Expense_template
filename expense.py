a960381cec37370723a55f9c15f883bdd1e3980e
aksel kuru <aksel.kuru@epita.fr>
2021-11-03 21:26:50 +0100

    Add ALL

--- expense.py
+++ expense.py
@@ -1,4 +1,5 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
@@ -16,15 +17,43 @@ expense_questions = [
         "name":"spender",
         "message":"New Expense - Spender: ",
     },
-
+    {
+        "type":"input",
+        "name":"people involved",
+        "message":"New Expense - Invovled people: ",
+    },
 ]
 
+def is_user(user):
+    with open('users.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 
+        for row in reader:
+            if row == user:
+                return True
+
+    return False
+
+def awesome_prompt():
+    infos = prompt(expense_questions)
+    return infos
 
 def new_expense(*args):
-    infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    infos = awesome_prompt()
+    
+    # L'optimisation s'est fait isekai par truck-kun :
+    # https://ih1.redbubble.net/image.761262045.2536/flat,750x,075,f-pad,750x1000,f8f8f8.jpg
+    while not(is_user(infos["spender"])):
+        print("The spender does not exist. Try with an existing user.")
+        infos = awesome_prompt()
+
+    with open('expense_report.csv', 'a') as f:
+        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+        l = []
+        for elem in infos:
+            l.append(infos[elem])
+        writer.writerow(some_input for some_input in l)
+
     print("Expense Added !")
     return True
-
-
--- user.py
+++ user.py
@@ -1,8 +1,23 @@
+from os import name
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"user_name",
+        "message":"New User",
+    }
 ]
 
-def add_user():
+def add_user(*args):
     # This function should create a new user, asking for its name
+    infos = prompt(user_questions)
+
+    with open('users.csv', 'a') as f:
+        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for elem in infos:
+            writer.writerow([infos[elem]])
+
+    print("User Created !")
     return
